3d269265d9e9d8e8adda998b68b8bd89c8982ca9
cod3r12 <85259706+cod3r12@users.noreply.github.com>
2024-08-13 10:15:53 -0500

    Add breathing exercises under projects

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ projectInfo = [
     // [title, description, image path, github link, languages]
     ["Running Log App", "A running log app made with flutter featuring many capabilities such as adding sets to your workout, custom routes, and more. This is one of my main ongoing projects at the moment.", "assets/runningLogApp.png", "https://github.com/liamb09/running_log", ["flutter"]],
     ["LogRun", "This simple running log was one of the first websites I made. It allows you to enter the details of your run, as well as customize the background color and text color on the display page.", "assets/logrun.png", "https://github.com/liamb09/LogRun", ["html", "css", "js"]],
+    ["Breathing Exercises", "A Garmin app (specifically developed for the Forerunner 55) that guides you through breathing exercises. At the moment, it has box breathing and 4-7-8 breathing.", "assets/breathingExercises.png", "https://github.com/liamb09/Garmin-Breathing-Exercises", ["connectIQ"]],
     ["Conway's Game of Life", "This is a website I made a while back that simulates the famous Conway's Game of Life.", "assets/conwaysGameOfLife.png", "https://github.com/liamb09/Conway-s-Game-of-Life", ["html", "css", "js"]],
     ["Swimming Scorer", "This Python program takes your time in some swimming event and returns your time standard, as well has how much time you need to drop to reach the next standard.", "assets/python.svg", "https://github.com/liamb09/Swimming-Times", ["python"]],
     ["Python Wordplay Projects", "This is a collection of various python projects I have made, including an anagram finder, a palingram finder, a spelling bee solver, a wordle solver, and a pig latin translator, as of now.", "assets/python.svg", "https://github.com/liamb09/Fun-Wordplay-Projects", ["python"]],
@@ -10,7 +11,7 @@ projectInfo = [
     ["Pong", "This was a simple practice program I made to get used to Godot for my Algebra II final project.", "assets/pong.png", "https://github.com/liamb09/Pong-in-Godot", ["godot"]],
     ["Algebra II Final Project", "This was my Algebra II class's final project, in which I was part of the team that developed games for an arcade cabinet using Godot. My game was similar to Deathball.", "assets/alg2FinalProject.jpeg", "https://github.com/liamb09/Algebra-2-Final-Project-Games", ["godot"]],
     ["Portfolio Website", "This is the very website you are viewing right now! It is made with HTML, CSS, and vanilla Javascript.", "assets/website.png", "https://github.com/liamb09/website", ["html", "css", "js"]],
-    ["Blog", "This is the blog I made with Jekyll, which can be found by clicking \"Blog\" at the top of this page.", "assets/blog.png", "https://github.com/liamb09/blog", ["jekyll"]],
+    ["Blog", "This is the blog I made with Jekyll, which can be found by clicking \"Blog\" at the top of this page. You can find out more about how I made it by reading the second blog post.", "assets/blog.png", "https://github.com/liamb09/blog", ["jekyll"]],
 ]
 
 function openDialog (infoID) {
@@ -37,6 +38,7 @@ function getIcons (projectID) {
 window.onload = function() {
     var projects = "";
     for (var i = 0; i < projectInfo.length; i++) {
+        console.log(i);
         projects += `
                     <div id="project-card" onclick="openDialog(${i})">
                         <h2>${projectInfo[i][0]}</h2>
